46ff3583afbde40c5d9dd8c0680aa236f4d5c082
Gregory Jean-Baptiste <gregoryjean-baptiste@Gregorys-MacBook-Pro.local>
2015-11-02 16:16:51 -0500

    Annotates MobileLoginScreen.java

--- Eclipse/Design/src/Client/Mobile/MobileLoginScreen.java
+++ Eclipse/Design/src/Client/Mobile/MobileLoginScreen.java
@@ -10,16 +10,16 @@ public class MobileLoginScreen extends MobileScreen {
 	 }
 
 	/**
-	 * 
+	 * Initiates the authentication process for the user
 	 */
 	public void clickLogin() { 
 		// TODO Auto-generated method
 	 }
 
 	/**
-	 * 
-	 * @param username 
-	 * @param password 
+	 * Sets the username and password entered through the interface
+	 * @param username The user name associated with a Pint account
+	 * @param password The password for the account associated with the user name
 	 */
 	public void setValues(String username, String password) { 
 		// TODO Auto-generated method
