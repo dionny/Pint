af3c10ea4ed661cfbeb1c85eee6313f41c44a29b
Gregory Jean-Baptiste <gregoryjean-baptiste@Gregorys-MacBook-Pro.local>
2015-11-02 15:21:34 -0500

    Annotates DetailScreen.java

--- Eclipse/Design/src/Client/Mobile/DetailScreen.java
+++ Eclipse/Design/src/Client/Mobile/DetailScreen.java
@@ -11,22 +11,27 @@ public class DetailScreen extends MobileScreen {
 	public BloodDrive bloodDrive;
 
 	/**
-	 * Getter of bloodDrive
+	 * Getter of bloodDrive.
+	 * @return The BloodDrive displayed in the DetailScreen.
 	 */
 	public BloodDrive getBloodDrive() {
 	 	 return bloodDrive; 
 	}
 
 	/**
-	 * Setter of bloodDrive
+	 * Setter of bloodDrive.
+	 * @param The BloodDrive that is to be displayed in the DetailScreen.
 	 */
 	public void setBloodDrive(BloodDrive bloodDrive) { 
 		 this.bloodDrive = bloodDrive; 
 	}
 
 	/**
-	 * 
-	 * @param note 
+	 * After the user taps a notification in the notification list,
+	 * this method will scroll to that same notification on the 
+	 * DetailScreen. It will also open the contents for the user to
+	 * read.
+	 * @param The notification to scroll to and open. 
 	 */
 	public void scrollToAndOpen(Notification note) { 
 		// TODO Auto-generated method
