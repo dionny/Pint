31664ffc0fb2b82d7061dfc0abd622d6d2e7f5fa
Gregory Jean-Baptiste <gregoryjean-baptiste@Gregorys-MacBook-Pro.local>
2015-11-02 21:42:21 -0500

    Adds javadoc for client/web code

--- Eclipse/Design/src/Client/Web/CoordinatorDashboard.java
+++ Eclipse/Design/src/Client/Web/CoordinatorDashboard.java
@@ -12,45 +12,45 @@ public class CoordinatorDashboard extends WebScreen {
 	 }
 
 	/**
-	 * 
+	 * Displays a list of nurses not currently assigned to the selected blood drive.
 	 */
 	public void showAssignNursesPopup() { 
 		// TODO Auto-generated method
 	 }
 
 	/**
-	 * 
-	 * @param employee 
+	 * Adds selected nurse to the currently selected blood drive
+	 * @param employee The nurse to be added to the currently selected blood drive.
 	 */
 	public void addNurse(Employee employee) { 
 		// TODO Auto-generated method
 	 }
 
 	/**
-	 * 
-	 * @param employee 
+	 * Saves nurse assignment for currently selected blood drive.
+	 * @param employee The nurse that is to be saved.
 	 */
 	public void submitNurses(Employee employee) { 
 		// TODO Auto-generated method
 	 }
 
 	/**
-	 * 
-	 * @param employee 
+	 * Confirms the addition of a nurse to a blood drive
+	 * @param employee The nurse that is confirmed by the user
 	 */
 	public void confirmSubmitNurses(Employee employee) { 
 		// TODO Auto-generated method
 	 }
 
 	/**
-	 * 
+	 * Displays a confirmation window before saving nurse assignments to a blood drive
 	 */
 	public void displayConfirmation() { 
 		// TODO Auto-generated method
 	 }
 
 	/**
-	 * 
+	 * Updates the screen with information regarding blood drives
 	 */
 	public void refresh() { 
 		// TODO Auto-generated method
--- Eclipse/Design/src/Client/Web/NurseDashboard.java
+++ Eclipse/Design/src/Client/Web/NurseDashboard.java
@@ -3,21 +3,21 @@ package Client.Web;
 public class NurseDashboard extends WebScreen {
 
 	/**
-	 * 
+	 * Adds donor information to the associated blood drive
 	 */
 	public void addDonor() { 
 		// TODO Auto-generated method
 	 }
 
 	/**
-	 * 
+	 * Displays the form used to add a donor to the associated blood drive
 	 */
 	public void displayAddDonorPopup() { 
 		// TODO Auto-generated method
 	 }
 
 	/**
-	 * 
+	 * Sets the value of the email address of the current donor.
 	 * @param emailAddress 
 	 */
 	public void enterEmail(String emailAddress) { 
@@ -25,28 +25,28 @@ public class NurseDashboard extends WebScreen {
 	 }
 
 	/**
-	 * 
+	 * Submits form information for further processing
 	 */
 	public void submit() { 
 		// TODO Auto-generated method
 	 }
 
 	/**
-	 * 
+	 * Displays a confirmation for the user before allowing donor information to be submitted
 	 */
 	public void displayConfirmation() { 
 		// TODO Auto-generated method
 	 }
 
 	/**
-	 * 
+	 * Accepts user confirmation for submitting donor information.
 	 */
 	public void confirm() { 
 		// TODO Auto-generated method
 	 }
 
 	/**
-	 * 
+	 * Displays a list of blood drives assigned to the current nurse
 	 */
 	public void viewBloodDrives() { 
 		// TODO Auto-generated method
