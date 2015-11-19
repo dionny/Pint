d512de5b451f0ec7f582fdc842ac9118e6966bfb
Azizul Hakim <Azizul Hakim>
2015-11-19 00:57:00 -0500

    add some entity class with hibernate annotation

---
+++ Web/src/main/java/com/example/Employee.java
@@ -0,0 +1,40 @@
+package com.example;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
+@Entity
+@Table(name="employee")
+public class Employee {
+	@Id
+	@NotNull
+	private String emailAddress;
+	
+	@NotNull
+	@Size(min = 1, max = 10)
+	private String role;
+	
+	@NotNull
+	@Size(min = 1, max = 50)
+	private String fName;
+	
+	@NotNull
+	@Size(min = 1, max = 50)
+	private String lName;
+	
+	@NotNull
+	@Size(min = 1, max = 15)
+	private String phoneNo;
+	
+	@NotNull
+	@Size(min = 1, max = 15)
+	private String password;
+	
+	@OneToOne(targetEntity=Hospital.class)
+	private long hospitalId;
+}
---
+++ Web/src/main/java/com/example/Hospital.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Hospital {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long hospitalId;
+	  
+	@NotNull
+	@Size(min = 3, max = 200)
+	private String hospitalName;
+}
